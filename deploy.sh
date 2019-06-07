3a4bf906c18bbe5f5ef717540a06b3a296ceabde
Marko Grujic <45558892+marko-grujic-frame@users.noreply.github.com>
2019-06-07 14:01:48 +0200

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcloud deployment-manager deployments delete byo-test --quiet &>/dev/null
+gcloud deployment-manager deployments create byo-test --config byo.yml --project $1
+gcloud iam service-accounts keys create byo_creds.json --iam-account=frame-byo-workload@$1.iam.gserviceaccount.com
