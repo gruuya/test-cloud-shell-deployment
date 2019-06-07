25053c82c75d875a2590ff28e56e8d2f820f3a3a
Marko Grujic <45558892+marko-grujic-frame@users.noreply.github.com>
2019-06-07 14:49:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gcloud deployment-manager deployments delete byo-test --quiet &>/dev/null
 gcloud deployment-manager deployments create byo-test --config byo.yml --project $1
-gcloud iam service-accounts keys create byo_creds.json --iam-account=frame-byo-workload@$1.iam.gserviceaccount.com
+gcloud iam service-accounts keys create ../byo_creds.json --iam-account=frame-byo-workload@$1.iam.gserviceaccount.com
+cloudshell download-file byo_creds.json
