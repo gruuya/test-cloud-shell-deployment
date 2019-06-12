4c84af640819af5c5a09b9f13b89546c7d3c5830
Marko Grujić <marko.grujic@nutanix.com>
2019-06-12 15:44:11 +0200

    Set project on firts line.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+gcloud config set project $1
 gcloud services enable deploymentmanager.googleapis.com
 gcloud deployment-manager deployments delete byo-test --quiet &>/dev/null
-gcloud deployment-manager deployments create byo-test --config byo.yml --project $1
+gcloud deployment-manager deployments create byo-test --config byo.yml
 cd .. && rm -fR test-cloud-shell-deployment
