2dc7c7575bdaab947d743e1539724baf98335a32
Marko Grujić <marko.grujic@nutanix.com>
2019-06-12 16:27:20 +0200

    Test explicit role setting.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-gcloud config set project $1
-gcloud services enable cloudresourcemanager.googleapis.com
-gcloud services enable deploymentmanager.googleapis.com
-gcloud deployment-manager deployments delete byo-test --quiet &>/dev/null
-gcloud deployment-manager deployments create byo-test --config byo.yml
+# gcloud config set project $1
+# gcloud services enable cloudresourcemanager.googleapis.com
+# gcloud services enable deploymentmanager.googleapis.com
+# gcloud deployment-manager deployments delete byo-test --quiet &>/dev/null
+# gcloud deployment-manager deployments create byo-test --config byo.yml
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.instanceAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.networkAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.securityAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.storageAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/dns.admin
 cd .. && rm -fR test-cloud-shell-deployment
