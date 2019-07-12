c1eff4c1edfc2a9e6ec87e1a31783efc4630cee2
Marko Grujić <marko.grujic@nutanix.com>
2019-07-12 09:18:00 +0200

    Setting staging service account in the deployment script in a new branch.

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 # gcloud services enable deploymentmanager.googleapis.com
 # gcloud deployment-manager deployments delete byo-test --quiet &>/dev/null
 # gcloud deployment-manager deployments create byo-test --config byo.yml
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.instanceAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.networkAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.securityAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/compute.storageAdmin
-gcloud projects add-iam-policy-binding $1 --member=serviceAccount:1018189488659-compute@developer.gserviceaccount.com --role=roles/dns.admin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.instanceAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.networkAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.securityAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/compute.storageAdmin
+gcloud projects add-iam-policy-binding $1 --member=serviceAccount:frame-stage@frame-customer-iaas-stage.iam.gserviceaccount.com --role=roles/dns.admin
 cd .. && rm -fR test-cloud-shell-deployment
